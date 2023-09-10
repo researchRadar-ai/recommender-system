52f3842ee5e671911dae763428cd8cfd4f51ab39
Ziping Ye <ziping.d.ye@gmail.com>
2023-09-09 21:56:12 -0400

    update dataset generation code

--- create_dummy_dataset.py
+++ create_dummy_dataset.py
@@ -9,7 +9,7 @@ import pandas as pd
 '''
     ProjectID PaperID Rating(0-10) Category Title Citation_Count Impact_Factor Time_Spent Number_of_Annotations
 '''
-def build_dataset(size):
+def build_dataset_full(size):
 
     dataset = pd.DataFrame(columns=['ProjectID', 'PaperID', 'Rating', 'Category', 'Title', 'Citation_Count', 'Impact_Factor', 'Time_Spent', 'Number_of_Annotations'])
     for i in range(size):
@@ -25,6 +25,23 @@ def build_dataset(size):
                           ]
     
     dataset.to_csv('dataset.csv', index=False)
+
+
+'''
+    userID itemID rating timestamp
+'''
+def build_dataset(size):
+
+    dataset = pd.DataFrame(columns=['userID', 'itemID', 'rating', 'timestamp'])
+    for i in range(size):
+        dataset.loc[i] = [random.randint(1, 100), # userID 
+                          random.randint(1, 1000), # itemID
+                          random.randint(1, 10), # rating
+                          f'{random.randint(2015, 2020)}-0{random.randint(1, 9)}-0{random.randint(1, 25)}' # timestamp
+                          ]
+    
+    dataset.to_csv('data/dataset.csv', index=False)
+
 
 
 if __name__ == '__main__':
